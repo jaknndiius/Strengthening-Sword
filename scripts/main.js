552ed09846914b13da97122c61ba3321a14492c0
JUNAisKIND <0627juna@naver.com>
2022-10-09 03:38:12 +0900

    inventory, swrodic

--- scripts/main.js
+++ scripts/main.js
@@ -2,77 +2,95 @@ const $ = (selector) => document.querySelector(selector)
 
 const GameManager = {
   swords: [],
+  max_upgradable_count: 30,
   sword_index: 0,
+  max_sword_index: 0,
   money: 100000,
   repair_paper: 0,
   inventory: [],
   records: [],
   max_recordable_count: 10
 }
+GameManager.resetSword = function() {
+  this.sword_index = 0;
+}
+GameManager.upgradeSword = function() {
+  if(++this.sword_index > this.max_sword_index) this.max_sword_index = this.sword_index;
 
+}
 GameManager.canBeRepaired = function() {
   return this.repair_paper >= this.getCurrentSword().requiredRepairs;
 }
 GameManager.getCurrentSword = function() {
-  return this.swords[this.sword_index]
+  return this.swords[this.sword_index];
 }
 GameManager.appendSword = function(sword) {
   this.swords.push(sword);
 }
 GameManager.calculateLoss = function(index) {
   return this.swords.filter((value, idx) => idx <= index)
-      .reduce((pre, cur) => pre += cur.cost, 0)
+      .reduce((pre, cur) => pre += cur.cost, 0);
 }
 GameManager.renderGameInterFace = function() {
 
-  const sell_button = $("#sell-button")
+  const sell_button = $("#sell-button");
 
-  const current_sword = this.getCurrentSword()
+  const current_sword = this.getCurrentSword();
 
-  $("#sword-image").src = current_sword.image
+  $("#sword-image").src = current_sword.image;
 
-  $("#sword-number").textContent = this.sword_index + "강"
-  $("#sword-name").textContent = current_sword.name
-  $("#sword-prob").textContent = "강화 성공 확률: " + Math.floor(current_sword.prob *100) + "%"
-  $("#sword-cost").textContent = "강화 비용: " + current_sword.cost + "원"
-  $("#sword-price").textContent = "판매 가격: " + current_sword.price + "원"
+  $("#sword-number").textContent = this.sword_index + "강";
+  $("#sword-name").textContent = current_sword.name;
+  $("#sword-prob").textContent = "강화 성공 확률: " + Math.floor(current_sword.prob *100) + "%";
+  $("#sword-cost").textContent = "강화 비용: " + current_sword.cost + "원";
+  $("#sword-price").textContent = "판매 가격: " + current_sword.price + "원";
   
-  if(this.sword_index === 0) {
-      sell_button.style.display = "none"
-  } else {
-      sell_button.style.display = "block"
-  }
+  sell_button.style.display = (this.sword_index === 0) ? "none" : "block";
+
 }
 GameManager.renderGameInformation = function() {
 
-}
-GameManager.renderInventory = function () {
+  const found = this.swords.slice(0, this.max_sword_index +1)
+    .map(value => `<div class="sword_icon"><img src="${value.image}" alt="${value.name}"></div>`);
+  $("#found-sword-count").textContent = found.length;
 
+  const unknown = this.max_upgradable_count - found.length;
+  for(let i = 0; i<unknown; i++) {
+    found.push(`<div class="sword_icon"><img src="images/swords/unknown.png" alt="none"></div>`);
+  }
+  $("#found-swords").innerHTML = found.join("");
+}
+GameManager.renderInventory = function() {
+
+}
+GameManager.returnDroppedPieces = function(name, count) {
+  return {name: name, count: count};
 }
 GameManager.renderFallMessage = function(...pieces) {
-  const pieces_box = $("#pieces")
+  const pieces_box = $("#pieces");
 
-  $("#loss").textContent = "손실: " + this.calculateLoss(this.sword_index) + "원"
-  pieces_box.innerHTML = ""
+  $("#loss").textContent = "손실: " + this.calculateLoss(this.sword_index) + "원";
+  pieces_box.innerHTML = "";
+
   pieces.forEach(
       ele => {
           const p = document.createElement("p")
           p.textContent = ele.name + " x " + ele.count
           pieces_box.appendChild(p)
       }
-  )
+  );
 }
 GameManager.changeGold = function(number) {
 
-  const gold_change_span = $("#gold-change")
+  const gold_change_span = $("#gold-change");
 
-  this.money += number
+  this.money += number;
 
   if(this.money < 0) {
-    this.money = 0
+    this.money = 0;
   }
 
-  gold_change_span.textContent = ((number >= 0) ? "+" + number : number) + "원"
+  gold_change_span.textContent = ((number >= 0) ? "+" + number : number) + "원";
 
   gold_change_span.animate(
     [
@@ -80,62 +98,62 @@ GameManager.changeGold = function(number) {
       {opacity: '0', transform: 'translate(-50%, -70%)'}
     ],
     {duration: 300, fill: "both"}
-  )
+  );
 
-  this.renderGold()
+  this.renderGold();
 
-  return true
+  return true;
 }
 GameManager.renderGold = function() {
   $("#gold-number").textContent = this.money;
 }
 GameManager.addRecord = function(sword, type) {
-  this.records.push({sword: sword, type: type})
-  let nrecs = [...this.records]
-  this.records = nrecs.reverse().filter((value, index) => index < this.max_recordable_count);
-  this.records.reverse()
+  this.records.push({sword: sword, type: type});
+  let idx = this.records.length - this.max_recordable_count;
+  if(idx < 0) idx = 0;
 
-  this.renderRecords()
+  this.records = this.records.slice(idx);
+  this.renderRecords();
 }
 GameManager.renderRecords = function () {
-  const records_div = $("#records")
+  const records_div = $("#records");
 
-  records_div.innerHTML = ""
+  records_div.innerHTML = "";
   for(rec of this.records) {
-      const p = document.createElement("p")
+      const p = document.createElement("p");
       
       if(rec.type == "upgrade") {
           p.textContent = `${rec.sword.name} 강화 -${rec.sword.cost}`;
       } else if(rec.type == "sell")
       p.textContent =  `${rec.sword.name} 판매 +${rec.sword.price}`;
-      $("#records").appendChild(p)
+      $("#records").appendChild(p);
   }
 }
 GameManager.changeBody = function(id) {
-  $("#main-body").innerHTML = $("#" + id).innerHTML
+  $("#main-body").innerHTML = $("#" + id).innerHTML;
 }
 GameManager.showGameInterface = function() {
-  this.changeBody("game-interface")
-  this.renderGameInterFace()
+  this.changeBody("game-interface");
+  this.renderGameInterFace();
 }
 GameManager.showGameInformation = function() {
-  this.changeBody("game-information")
-  this.renderGameInformation()
+  this.changeBody("game-information");
+  this.renderGameInformation();
 }
 GameManager.showInventory = function() {
-  this.changeBody("inventory")
-  this.renderInventory()
+  this.changeBody("inventory");
+  this.renderInventory();
 }
 GameManager.popupFallMessage = function(...pieces) {
 
-  this.renderFallMessage(...pieces)
+  this.renderFallMessage(...pieces);
 
-  const message_box = $("#message-box")
-  message_box.style.display = "block"
+  const message_box = $("#message-box");
+  message_box.style.display = "block";
   message_box.animate(
       [{opacity: '0'}, {opacity: '1'}],
       {duration: 300, fill: "both"}
-  )
+  );
 }
 
 // sword class
@@ -158,16 +176,11 @@ class Piece {
       this.max_drop = max_drop;
   }
 
-  /**
-   * 
-   * @returns 조각 드랍 성공시 파편 이름과 1~this.max_drop중 무작위 수 반환
-   */
-  caculate() {
-      
+  calculate() {
       if(Math.random() < this.prob) {
-          const num = Math.round(Math.random()*100);
-          const result = Math.ceil(num/(100/this.max_drop));
-          return {name: this.name, count: result};
+          // const num = Math.round(Math.random()*100);
+          const result = Math.ceil(Math.round(Math.random()*100)/(100/this.max_drop));
+          return GameManager.returnDroppedPieces(this.name, result);
       } return null;
   }
 }
@@ -178,10 +191,10 @@ const asdf = [
   new Sword("처형인의 대검", 0.9, 400, 300, 1),
   new Sword("BF 대검", 0.85, 500, 600, 1),
   new Sword("마나무네", 0.8, 600, 1000, 1),
-  new Sword("무라마나", 0.75, 700, 1200, 1),
-  new Sword("드락사르의 황혼검", 0.7, 1000, 1500, 1),
-  new Sword("무한의 대검", 0.65, 1500, 3000, 1),
-  new Sword("수호 천사", 0.6, 5000, 12000, 1),
+  new Sword("무라마나", 0.75, 700, 1200, 1, new Piece("name", 1.0, 10), new Piece("name2", 1.0, 100)),
+  new Sword("드락사르의 황혼검", 0.7, 1000, 1500, 1, new Piece("name", 1.0, 10), new Piece("name2", 1.0, 100)),
+  new Sword("무한의 대검", 0.65, 1500, 3000, 1, new Piece("name", 1.0, 10), new Piece("name2", 1.0, 100)),
+  new Sword("수호 천사", 0.6, 5000, 12000, 1, new Piece("name", 1.0, 10), new Piece("name2", 1.0, 100)),
   new Sword("제국의 명령", 0.55, 10000, 30000, 1),
   new Sword("요우무의 유령검", 0.5, 300, 0, 1),
   new Sword("톱날 단검", 0.45, 300, 100, 1),
@@ -195,29 +208,32 @@ const asdf = [
   new Sword("구인수의 격노검", 0.5, 10000, 30000, 1)
 ]
 
-asdf.forEach(value => GameManager.appendSword(value))
+asdf.forEach(value => GameManager.appendSword(value));
 
 // 초기화 및 전체 업데이트
 function init() {
-  GameManager.sword_index = 0
-  GameManager.showGameInterface()
-  GameManager.renderGold()
+  GameManager.resetSword();
+  GameManager.showGameInterface();
+  GameManager.renderGold();
 }
 
 // onClick: upgrade button
 function upgrade() {
-  const current_sword = GameManager.getCurrentSword()
 
-  GameManager.addRecord(current_sword, "upgrade")
-  GameManager.changeGold(-current_sword.cost)
+  const current_sword = GameManager.getCurrentSword();
+  if(GameManager.money - current_sword.cost <= 0) return;
 
-  const num = Math.random()
-  console.log(num, current_sword.prob)
+  console.log(GameManager.max_sword_index);
+  GameManager.addRecord(current_sword, "upgrade");
+  GameManager.changeGold(-current_sword.cost);
+
+  const num = Math.random();
+  console.log(num, current_sword.prob);
 
   if(num < current_sword.prob) {
-      GameManager.sword_index++
+      GameManager.upgradeSword();
   } else {
-      const re = current_sword.pieces.map(value => value.caculate())
+      const re = current_sword.pieces.map(value => value.calculate());
 
       re.forEach(value => {
           if(value.name in GameManager.inventory) { //인벤토리에 있으면
@@ -225,46 +241,46 @@ function upgrade() {
           } else { //없으면
             GameManager.inventory[value.name] = value.count
           }
-      })
-      GameManager.popupFallMessage(...re)
+      });
+      GameManager.popupFallMessage(...re);
   }
   
-  GameManager.renderGameInterFace()
+  GameManager.renderGameInterFace();
 }
 
 // onClick: sell button
 function sell() {
 
-  GameManager.addRecord(GameManager.getCurrentSword(), "sell")
-  GameManager.changeGold(GameManager.getCurrentSword().price)
+  GameManager.addRecord(GameManager.getCurrentSword(), "sell");
+  GameManager.changeGold(GameManager.getCurrentSword().price);
   
-  init()
+  init();
 }
 
 // onClick: init button
 function initButton() {
-  $("#message-box").style.display = "none"
-  init()
+  $("#message-box").style.display = "none";
+  init();
 }
 
 // onClick: fix button
 function repair() {
-  console.log(GameManager.canBeRepaired())
+  console.log(GameManager.canBeRepaired());
 }
 
 /* Footer */
 
 $("#main-game-button").addEventListener("click", () => {
-  GameManager.showGameInterface()
+  GameManager.showGameInterface();
 })
 $("#information-button").addEventListener("click", () => {
-  GameManager.showGameInformation()
+  GameManager.showGameInformation();
 })
 $("#inventory-button").addEventListener("click", () => {
-  GameManager.showInventory()
+  GameManager.showInventory();
 })
 $("#making-button").addEventListener("click", () => {
 
 })
 
-init()
+init();
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 /*
 
-class
+---Class---------------------------------
 
 new Sword(
-  name:String,    :검의 이름
-  prob:Number,    :다음 단계 성공 확률
-  cost:Number,    :다음 단계 강화비용
-  price: Number,  :판매시 가격
-  ...pieces: ...Piece :드랍되는 조각들
+  name:String,    : 검의 이름
+  prob:Number,    : 다음 단계 성공 확률
+  cost:Number,    : 다음 단계 강화비용
+  price: Number,  : 판매시 가격
+  ...pieces: ...Piece : 드랍되는 조각들
 )
 new Piece(
   name:String,    : 조각 이름
@@ -15,29 +15,41 @@ new Piece(
   max_drop:Number : 최대 생성 확률
 )
 
+---Funcion&Variable---------------------------------
+
 Piece.caculate() : Object?
   생성 확률에 따라 성공시 {name: String, count: Number}를, 실패시 null 반환
   name: 조각 이름
   count: 생성된 조각(prob 성공시 1~max_drop까지는 동일한 확률로 생성됨)
 
+init() : 게임을 초기화한다(0강으로 되돌림)
 
+$(selector: String) : Element | selector로 찾은 html 요소를 반환한다.
 
+GameManager : Object | 게임관련 변수와 함수를 가지고 있는 객체
+GameManager.swords : Sword[] | 검을 담고 있는 배열
+GameManager.sword_index : Number | 현재 몇강인지 담고 있는 정수
+GameManager.money : Number | 현재 보유 자산을 담고 있는 변수
+GameManager.repair_paper : Number | 현재 가진 복구권 갯수
+GamaManager.inventory : Object[] | 보유한 조각, 복구권을 담고 있는 배열
+GamaManager.records : Object[] | 최근 강화/판매 전적을 담고 있는 배열
+@ReadOnly GamaManager.max_recordable_count : Number | 최대 기록(GameManager.records에 담기는) 가능한 갯수
 
-init() : 검을 0강으로 되돌린다
-
+GameManager.canBeRepaired() : Boolean | 현재 가진 복구권으로 복구가 가능한지 반환한다.
 GameManager.getCurrentSword() : Sword | 현재 검을 반환한다.
 GameManager.appendSword(sword:Sword) | 검을 하나 추가한다.
 GameManager.calculateLoss(index:Number) : Number | index를 포함한 검까지 강화하는데 소모된 골드를 계산함.
 GameManager.renderGameInterFace() | 검강화 화면을 새로고침 한다.
 GameManager.renderGameInformation() | 내 정보 화면을 새로고침 한다.
 GameManager.renderInventory() | 보관함 화면을 새로고침 한다.
-GameManager.renderFallMessage(...pieces: ...Pieces) | 드랍된 조각을 포함한  실패 화면을 새로고침한다.
+GameManager.returnDroppedPieces(name: String, count: Number) : Object | {name: name, count: count} 오브젝트로 변경하여 반환한다.
+GameManager.popupFallMessage(...pieces : ...Pieces) | 드랍된 조각이 포함된 실패창을 새로고침 한 뒤 띄운다. (pieces는 returnDroppedPieces() 로 반환된 Object의 배열이여야 한다.)
 GameManager.changeGold(Number) | 보유 자산을 Number 만큼 가감한다.
-GameManager.addRecord(Sword, *Mod) *"upgrade" || "sell" | 최근 기록에 추가한다.
+GameManager.addRecord(sword: Sword, *mod: String) *"upgrade" || "sell" | 최근 기록에 추가한다.
 
-showGameInterface() | 검강화 화면을 새로고침한 후 띄운다.
-showGameInformation() | 내정보 화면을 새로고침 한 후 띄운다.
-showInventory() | 보관함 화면을 새로고침 한 후 띄운다.
-popupFallMessage(...pieces) | 
+*GameManager.showGameInterface() | 검강화 화면을 새로고침한 후 띄운다.
+*GameManager.showGameInformation() | 내정보 화면을 새로고침 한 후 띄운다.
+*GameManager.showInventory() | 보관함 화면을 새로고침 한 후 띄운다.
+*GameManager.renderFallMessage(...pieces: ...Pieces) | 드랍된 조각을 포함한  실패 화면을 새로고침한다.
 
 */
