7d947a05d9555026210a716b3ed434eee5fcd7be
JUNAisKIND <0627juna@naver.com>
2022-11-26 02:52:59 +0900

    order

--- scripts/other/setting.js
+++ scripts/other/setting.js
@@ -1,13 +1,8 @@
-/**
- * 
- * @param {} selector 
- * @returns Element 
- */
 const $ = (selector) => document.querySelector(selector);
 const $createElementWithClasses = (tagName, ...classes) => { const tag = document.createElement(tagName); tag.classList.add(...classes); return tag; };
 const $createImgWithSrc = (src, alt="") => { const img = new Image(); img.src = src; img.alt = alt; return img; };
-Node.prototype.appendChildren = function(...nodes) { for(const node of nodes ) this.appendChild(node); };
 Node.prototype.text = function(textContent) { this.textContent = textContent; return this; }
+Node.prototype.appendChildren = function(...nodes) { for(const node of nodes ) this.appendChild(node); };
 HTMLElement.prototype.display = function() { this.style.display = "block" };
 HTMLElement.prototype.hide = function() { this.style.display = "none" };
 const TestResult = {
